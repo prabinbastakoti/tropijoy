7401c2372f6b16d6802dad7bd8abee14a0c22e55
Prabin Bastakoti <prabinbastakoti1@gmail.com>
2026-08-19 00:25:16 +0545

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="page">
+      <img className="bg-fill" src="/bg.png" alt="" aria-hidden="true" />
+      <img className="bg-image" src="/bg.png" alt="" />
+    </div>
+  )
+}
